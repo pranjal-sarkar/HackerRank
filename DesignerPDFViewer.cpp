decc5499ed8aff098682a3ad3a645eb336b59044
Pranjal <tataisarkar2001@gmail.com>
2021-09-23 19:41:44 +0530

    DesignerPDFViewer.cpp Source Code

--- DesignerPDFViewer.cpp
+++ DesignerPDFViewer.cpp
@@ -3,36 +3,38 @@ using namespace std;
 
 #define height_count 26
 
-void solve(int[], char[]);
+// void solve(int[], char[]);
+
+void solve(int heights[], char letter[]){
+    int i=0;
+    int index;
+    int max_height=0;
+
+    while(letter[i]!='\0'){
+        index = letter[i] - 'a';
+        max_height = max(max_height, heights[index]);
+
+        i++;
+    }
+
+    cout<<max_height * i;
+}
 
 int main(){
     int heights[height_count];
     char letter[10];
-    int n, i;
+    int i;
 
-    for(i=0;i < height_count;i++){
+    for(i=0;i<height_count;i++){
         cin>>heights[i];
     }
+    fflush(stdin);
 
-    gets(letter);
+    // gets(letter);
+    cin>>letter;
 
     solve(heights, letter);
 
     return 0;
 }
 
-void solve(int heights[], char letter[]){
-    int i=0;
-    int curr_height=0;
-    int index;
-    int max_height;
-
-    while(letter[i]!='\0'){
-        index = letter[i] - 'a';
-        max_height = max(curr_height, heights[index]);
-
-        i++;
-    }
-
-    cout<<max_height*strlen(letter);
-}
